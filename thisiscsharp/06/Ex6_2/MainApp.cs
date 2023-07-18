6def33205b0b17386667959de241150a4e25a363
dhampir7 <dhampir7@naver.com>
2023-07-18 21:35:10 +0900

    7/18

---
+++ thisiscsharp/06/Ex6_2/MainApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex6_2
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            double mean = 0;
+
+            Mean(1, 2, 3, 4, 5, out mean);
+
+            Console.WriteLine("평균 : {0}", mean);
+        }
+
+        public static void Mean(
+            double a, double b, double c,
+            double d, double e, out double mean)
+        {
+            mean = (a + b + c + d + e) / 5;
+        }
+    }
+}
