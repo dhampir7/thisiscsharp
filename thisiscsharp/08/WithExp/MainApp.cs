9c5ff33a9c70929710ebb9fba3af7813e41890f4
관섭 김 <dhampir7@naver.com>
2023-07-27 16:50:43 +0900

    7/27

---
+++ thisiscsharp/08/WithExp/MainApp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace WithExp
+{
+    record RTransaction
+    {
+        public string From  { get; init; }
+        public string To    { get; init; }
+        public int Amount   { get; init; }
+
+        public override string ToString()
+        {
+            return $"{From,-10} -> {To,-10} : ${Amount}";
+        }
+    }
+
+    class MinApp
+    {
+        static void Main(string[] args)
+        {
+            CTransaction trA = new CTransaction { from = "Alice", To = "Bob", Amount = 100 };
+            CTransaction trB = new CTransaction { from = "Alice", To = "Bob", Amount = 100 };
+
+            Console.WriteLine(trA);
+            Console.WriteLine(trB);
+            Console.WriteLine($"trA equals to trB : {trA.Equals(trB)});
+
+            RTransaction tr1 = new RTransaction { From="Alice", To="Bob", Amount=100};
+            RTransaction tr2 = tr1 with { To = "Charlie"};
+
+            Console.WriteLine(tr1);
+            Console.WriteLine(tr2);
+            Console.WriteLine($"tr1 equals to tr2 : {tr1.Equals(tr2)});
+
+            
+        }
+    }
+}
