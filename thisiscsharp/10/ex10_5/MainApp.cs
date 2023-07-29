eed1691cd5b1f38a625bd763ec7c1bdb42661114
관섭 김 <dhampir7@naver.com>
2023-07-29 19:24:44 +0900

    7/29

---
+++ thisiscsharp/10/ex10_5/MainApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using static System.Console;
+
+namespace ex10_5
+{
+    class MainApp
+    {
+
+
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+
+            ht["회사"]  = "Microsoft";
+            ht["URL"] = "www.micorsoft.com";
+
+            Console.WriteLine("회사 : {0}", ht["회사"] );
+            Console.WriteLine("URL : {0}", ht["URL"] );
+        }
+    }
+}
---
+++ thisiscsharp/11/ConstraintsOnTypeParameters/MainApp.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public interface ICreatable
+{
+    void CreateInstance();
+}
+
+public class Base : ICreatable
+{
+    public void CreateInstance()
+    {
+        Console.WriteLine("Base instance created!");
+    }
+}
+
+public class Derived : Base, ICreatable
+{
+    public new void CreateInstance()
+    {
+        Console.WriteLine("Derived instance created!");
+    }
+}
+
+class BaseArray<U> where U : Base, ICreatable
+{
+    public U[] Array { get; set; }
+
+    public BaseArray(int size)
+    {
+        Array = new U[size];
+    }
+
+    public void CopyArray<T>(T[] Source) where T : U
+    {
+        Source.CopyTo(Array, 0);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BaseArray<Base> c = new BaseArray<Base>(3);
+        c.Array[0] = new Base();
+        c.Array[1] = new Derived();
+        c.Array[2] = new Derived();
+
+        BaseArray<Derived> d = new BaseArray<Derived>(3);
+        d.Array[0] = new Derived();
+        d.Array[1] = new Derived();
+        d.Array[2] = new Derived();
+
+        BaseArray<Derived> e = new BaseArray<Derived>(3);
+        e.CopyArray<Derived>(d.Array);
+
+        // Test CreateInstance method from Derived class
+        d.Array[0].CreateInstance();
+        d.Array[1].CreateInstance();
+        d.Array[2].CreateInstance();
+    }
+}
