590b91f9d72c8ed863742a41ef696a4c95cd28c1
관섭 김 <dhampir7@naver.com>
2023-07-30 15:38:28 +0900

    7/30

---
+++ thisiscsharp/11/UsingGenericList/MainApp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UsingGenericList
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < 5; i++)
+                list.Add(i);
+
+            foreach (int element in list)
+                Console.Write($"{element} ");
+
+            list.RemoveAt(2);
+
+            foreach (int element in list)
+                Console.Write($"{element} ");
+            Console.WriteLine();
+
+            list.Insert(2, 2);
+
+            foreach (int element in list)
+                Console.WriteLine($"{element} ");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ thisiscsharp/12/Ex12_1/MainApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ex12_1
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] arr = new int[10];
+
+                for (int i = 0; i < 10; i++)
+                    arr[i] = i;
+
+                for (int i = 0; i < 11; i++)
+                    Console.WriteLine(arr[i]);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
