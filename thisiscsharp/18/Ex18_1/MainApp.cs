91871aea7386402104f6a5f3720c7801e3079dd8
관섭 김 <dhampir7@naver.com>
2023-08-08 20:17:01 +0900

    8/8

---
+++ thisiscsharp/18/Ex18_1/MainApp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace ReadSeqNRandFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stream inStream = new FileStream("a.dat", FileMode.Open);
+
+            byte[] rbytes = new byte[8];
+            int bytesRead;
+
+            while ((bytesRead = inStream.Read(rbytes, 0, 8)) > 0)
+            {
+                long readValue = BitConverter.ToInt64(rbytes, 0);
+                Console.WriteLine("{0,-13} : 0x{1:X16} ", "Read Data", readValue);
+            }
+
+            inStream.Close();
+        }
+    }
+}
---
+++ thisiscsharp/18/Ex18_2/MainApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace BinaryFileExperiment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (BinaryWriter bw = new BinaryWriter(new FileStream("a.dat", FileMode.Create)))
+            {
+                // Write a long string to the file
+                string longString = new string('X', 300);
+                bw.Write(longString);
+            }
+
+            using (BinaryReader br = new BinaryReader(new FileStream("a.dat", FileMode.Open)))
+            {
+                Console.WriteLine($"File size : {br.BaseStream.Length} bytes");
+                string readString = br.ReadString();
+                Console.WriteLine($"Read String Length: {readString.Length}");
+            }
+        }
+    }
+}
